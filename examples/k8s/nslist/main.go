eda2fb5a19eddfb14a105d042795fef762ad939d
Dmitry Zinushin <zinushin@gmail.com>
2022-04-03 15:01:16 +0300

    k8s client-go example for get ns list improve

--- examples/k8s/nslist/main.go
+++ examples/k8s/nslist/main.go
@@ -12,12 +12,13 @@ import (
 
 func main() {
 	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-	log.Printf("kubeconfig: %s", kubeconfig)
+	log.Printf("kubeconfig file: %s", kubeconfig)
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		log.Fatalf("error build config: %v", config)
 	}
-	log.Println("k8s config created")
+	log.Printf("k8s config, host: %v", config.Host)
+
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Fatalf("error create clientset: %v", err)
@@ -25,8 +26,15 @@ func main() {
 
 	api := clientset.CoreV1()
 
+	log.Println("namespaces list:")
 	options := metav1.ListOptions{}
 	nsList, err := api.Namespaces().List(context.TODO(), options)
-	log.Printf("%v", nsList)
+	if err != nil {
+		log.Fatalf("error get list of namespaces: %v", err)
+	}
+
+	for _, ns := range nsList.Items {
+		log.Printf("ns: %v", ns.Name)
+	}
 
 }
